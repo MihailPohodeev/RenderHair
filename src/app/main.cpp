3a6dbaaec58df58003e4003e2ab3d99e7f52dbba
MihailPohodeev <mishapohodeev@yandex.ru>
2026-04-11 22:37:11 +0300

    REFACTOR : removed unnecessary files.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,1 @@
-#include <QApplication>
-#include "mainwindow.hpp"
-
-int main(int argc, char *argv[]) noexcept
-{
-    const QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
-    return QApplication::exec();
-}
+int main(int argc, char *argv[]) noexcept {}
